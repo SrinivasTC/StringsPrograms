10c658eba5a4d3630c197f5b96fba41f1be7d6b8
usrin <usrin@LAPTOP-I0S4AQSQ>
2023-04-11 08:06:22 +0530

    Stringpromas

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author usrin
+ *
+ */
+module stringprogramss {
+}
---
+++ src/swappingnumbers/Ada.java
@@ -0,0 +1,23 @@
+package swappingnumbers;
+
+public class Ada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i=1; // i=2;
+		int j=4;
+		
+		for(i=2;i<=16;i++) {
+		System.out.println(i);
+			i=i+j;
+			j--;// 4-21=16; 16+1=17 17+4=21  and -21+4=-17, -21+3=18,21-2=19,	21-1=20	
+		}
+		
+	}
+		
+	
+	}
+
+
